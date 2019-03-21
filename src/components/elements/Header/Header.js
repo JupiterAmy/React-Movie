bbf69d0147519030a09c98e7927537cfc37716e1
amritansh <amritans@cisco.com>
2019-03-21 11:19:22 +0530

    changed the logo

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/explore.png" alt="rmdb-logo"/>
+                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
             </div>
         </div>
     );
